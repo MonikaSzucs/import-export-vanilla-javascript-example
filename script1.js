f75353a3bfbfd4e5b631396804d21fde3f3ffd8e
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-08-24 17:35:40 -0700

    added files

---
+++ script1.js
@@ -0,0 +1,3 @@
+export let Name = (name) => {return "My name is " + name;}
+export let Address = (address) => {return "i live in " + address;}
+export let Age = (age) => {return "my age is "+ age;}
---
+++ script2.js
@@ -0,0 +1,4 @@
+import { Name, Address, Age} from './script1.js';
+let val = Name("John");
+let age = Age(26);
+console.log(val + " and " + age);
